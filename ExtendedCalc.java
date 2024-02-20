161660dcd0375fbe257217306587d3a535124740
Ranjita.H.Illur <01fe22bca195@kletech.ac.in>
2024-02-20 11:02:02 +0530

    Adding Two files

---
+++ ExtendedCalc.java
@@ -0,0 +1,11 @@
+public class ExtendedCalc {
+    float square(float operand_1){
+        return operand_1*operand_1;
+     }
+
+     float cube(float operand_1){
+        return square(operand_1)*operand_1;
+     }
+ 
+
+}
